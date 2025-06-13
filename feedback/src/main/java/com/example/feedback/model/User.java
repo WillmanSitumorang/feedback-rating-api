61f12f4d72a6ebc840f4a859e5a48cd0487ff248
Willman <willmansatria5@gmail.com>
2025-06-13 14:52:49 +0700

    model dan repo

---
+++ feedback/src/main/java/com/example/feedback/model/User.java
@@ -0,0 +1,19 @@
+package com.example.feedback.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Feedback> feedbacks;
+}

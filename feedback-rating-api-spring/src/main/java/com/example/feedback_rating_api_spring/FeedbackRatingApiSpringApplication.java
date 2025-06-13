700447bcd86b101a9bf3a21b57702462e3e2c2d4
Putra Adhli Falah <71480@fif.co.id>
2025-06-13 13:37:57 +0700

    generate new project

---
+++ feedback-rating-api-spring/src/main/java/com/example/feedback_rating_api_spring/FeedbackRatingApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.feedback_rating_api_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedbackRatingApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedbackRatingApiSpringApplication.class, args);
+	}
+
+}
